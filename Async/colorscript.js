8b47e46caeb9bc6a140116667158c5190a7c7af5
rahul_00 <degrarahul3@gmail.com>
2025-01-03 11:48:04 +0530

    Async/

---
+++ Async/colorscript.js
@@ -0,0 +1,25 @@
+// generate a random color
+
+const randomColor = function () {
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+let intervalId
+const startChangingColor = function () {
+    intervalId = setInterval(changebgColor, 1000)
+
+    function changebgColor() {
+        document.body.style.backgroundColor = randomColor()
+}}
+const stopChangingColor = function () {
+    clearInterval(intervalId)
+}
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
